64a75f0c53f19c431c0b4bedd987c52a456d2438
JIEMO <2569995914@qq.com>
2020-12-21 17:07:15 +0800

    update

--- js/趣刷领红包.js
+++ js/趣刷领红包.js
@@ -1,5 +1,5 @@
 //耗时15分钟
-launch(getPackageName("趣刷领红包视频极速版"));
+launch(getPackageName("趣刷领红包"));
 var sh = new Shell(true);
 id("frag_my_page").waitFor();
 for (i = 0; i < 70; i++) {
@@ -9,7 +9,7 @@ for (i = 0; i < 70; i++) {
     sleep(1000);
     toastLog("计数器：" + (i + 1));
 }
-sh.exec("am force-stop " + getPackageName("趣刷领红包视频极速版"));
+sh.exec("am force-stop " + getPackageName("趣刷领红包"));
 sleep(1000);
 sh.exit;
 toastLog("【趣刷领红包】已完成计划任务并退出APP！");
