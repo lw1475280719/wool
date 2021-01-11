19e95bc8aac13d6561d72c16ae73433dabe24ef5
JIEMO <2569995914@qq.com>
2021-01-11 20:44:37 +0800

    add

---
+++ js/笑谱推购.js
@@ -0,0 +1,13 @@
+//耗时1小时
+launch(getPackageName("笑谱推购"));
+var sh = new Shell(true);
+className("androidx.appcompat.app.ActionBar$Tab").clickable(true).selected(true).depth(12).waitFor();
+for (i = 0; i < 330; i++) {
+    toastLog("计数器：" + (i + 1));
+    sleep(10 * 1000);
+    swipe(500, 1600, 500, 250, 600);
+}
+sh.exec("am force-stop " + getPackageName("笑谱推购"));
+sleep(1000);
+sh.exit;
+toastLog("【笑谱推购】已完成计划任务并退出APP！");
