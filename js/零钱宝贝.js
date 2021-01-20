2fe354fcd8f08b1fe7606099e02ca471fd23af15
JIEMO <2569995914@qq.com>
2021-01-20 14:49:10 +0800

    add

---
+++ js/零钱宝贝.js
@@ -0,0 +1,22 @@
+//(受广告时长影响)耗时40-60分钟
+launch(getPackageName("零钱宝贝"));
+var sh = new Shell(true);
+text("我的钱包").waitFor();
+sleep(2000);
+text("领取").find().click();
+for (i = 0; i < 70; i++) {
+    id("tt_video_ad_close_layout").findOne().click();  
+    sleep(8000);   
+    clickable(true).depth(15).find().click();
+    sleep(2000);    
+    if(text("立即提现").exists()){
+        Tap(540,1800);//Tap(540,1680);
+        sleep(1000);
+        text("领取").find().click();
+    }
+    toastLog("计数器：" + (i + 1)+"/63");
+}
+sh.exec("am force-stop " + getPackageName("零钱宝贝"));
+sleep(1000);
+sh.exit;
+toastLog("【零钱宝贝】已完成计划任务并退出APP！");
