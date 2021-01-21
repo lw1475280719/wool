181a08c6df5b294bacccfacf64708cf4bbdbad7d
JIEMO <2569995914@qq.com>
2021-01-21 18:04:24 +0800

    update

--- js/趣赚清理.js
+++ js/趣赚清理.js
@@ -1,7 +1,7 @@
 //耗时30分钟
 launch(getPackageName("趣赚清理"));
 var sh = new Shell(true);
-id("t1").waitFor();
+id("ui").waitFor();
 sleep(1000);
 click("小视频");
 sleep(2000);
@@ -10,10 +10,9 @@ for (i = 1; i < 160; i++) {
     sleep(8000);
     Tap(900, 1650);
     sleep(1000);
-    id("ais").find().click();
-    id("kz").find().click();
-    id("ly").find().click();
-    
+    id("alf").find().click();
+    id("ll").find().click();
+
     sleep(1000);
     swipe(800, 1600, 800, 266, 500);
 
