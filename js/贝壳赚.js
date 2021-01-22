5e6147507b1064bf341aaed6f0379a56cceb85ad
JIEMO <2569995914@qq.com>
2021-01-23 00:39:53 +0800

    add

--- js/贝壳赚.js
+++ js/贝壳赚.js
@@ -1,46 +1,46 @@
 //耗时1小时
 launch(getPackageName("贝壳赚"));
 var sh = new Shell(true);
-sleep(10*1000);
-if(text("立即提现").exists()){
+sleep(10 * 1000);
+if (text("立即提现").exists()) {
     text("立即提现").waitFor();
     text("立即提现").find().click();
-}else{
+} else {
     text("IKhfxJ5bxI9dDEfXvNwMfSu+J55SRfkPX+AphjYQHXRxYfAAAAAElFTkSuQmCC").findOne().parent().click();
 }
 sleep(1000);
 text("0.3").findOne().parent().parent().click();
 sleep(1000);
-for(i=0;i<300;i++){
+for (i = 0; i < 300; i++) {
     text("看视频提现秒到账").find().click();
     sleep(5000);
-    if(className("android.widget.ImageView").exists()){
+    if (className("android.widget.ImageView").exists()) {
         back();
-    }else if(id("tt_video_ad_close_layout").exists()){        
-        id("tt_video_ad_close_layout").find().click();       
+    } else if (id("tt_video_ad_close_layout").exists()) {
+        id("tt_video_ad_close_layout").find().click();
     }
     sleep(1000);
-    toastLog("计数器："+(i+1));
+    toastLog("计数器：" + (i + 1));
 }
-sleep(50*1000);
-if(className("android.widget.ImageView").exists()){
+sleep(50 * 1000);
+if (className("android.widget.ImageView").exists()) {
     back();
-}else if(id("tt_video_ad_close_layout").exists()){        
-    id("tt_video_ad_close_layout").find().click();       
+} else if (id("tt_video_ad_close_layout").exists()) {
+    id("tt_video_ad_close_layout").find().click();
 }
 sleep(1000);
 back();
-for(j=0;j<250;j++){
+for (j = 0; j < 250; j++) {
     // clickable(true).depth(13).find().click();
     click("收益加速");
     sleep(5000);
-    if(className("android.widget.ImageView").exists()){
+    if (className("android.widget.ImageView").exists()) {
         back();
-    }else if(id("tt_video_ad_close_layout").exists()){        
-        id("tt_video_ad_close_layout").find().click();       
+    } else if (id("tt_video_ad_close_layout").exists()) {
+        id("tt_video_ad_close_layout").find().click();
     }
     sleep(1000);
-    toastLog("计数器："+(j+350));
+    toastLog("计数器：" + (j + 350));
 }
 
 
