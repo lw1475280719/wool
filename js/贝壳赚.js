d37f12e85639557cf040aac1f12c37de1cd45b76
JIEMO <2569995914@qq.com>
2021-01-26 17:22:01 +0800

    update

--- js/贝壳赚.js
+++ js/贝壳赚.js
@@ -1,18 +1,19 @@
-//耗时1小时
+//耗时30分钟
 launch(getPackageName("贝壳赚"));
 var sh = new Shell(true);
 sleep(10 * 1000);
 if (text("立即提现").exists()) {
     text("立即提现").waitFor();
-    text("立即提现").find().click();
+    className("android.view.View").text("立即提现").find().click();
 } else {
     text("IKhfxJ5bxI9dDEfXvNwMfSu+J55SRfkPX+AphjYQHXRxYfAAAAAElFTkSuQmCC").findOne().parent().click();
 }
 sleep(1000);
 text("0.3").findOne().parent().parent().click();
 sleep(1000);
-for (i = 0; i < 220; i++) {
+for (i = 0; i < 280; i++) {
     text("看视频提现秒到账").find().click();
+    text("观看并点击视频广告").find().click()
     sleep(5000);
     if (className("android.widget.ImageView").exists()) {
         back();
@@ -21,7 +22,7 @@ for (i = 0; i < 220; i++) {
         id("tt_video_ad_close_layout").find().click();
     }
     sleep(1000);
-    text("立即提现").find().click();
+    className("android.view.View").text("立即提现").find().click();
     toastLog("计数器：" + (i + 1));
 }
 sleep(50 * 1000);
@@ -33,19 +34,19 @@ if (id("tt_video_ad_close_layout").exists()) {
 }
 sleep(1000);
 back();
-for (j = 0; j < 250; j++) {
-    // clickable(true).depth(13).find().click();
-    click("收益加速");
-    sleep(5000);
-    if (className("android.widget.ImageView").exists()) {
-        back();
-    }
-    if (id("tt_video_ad_close_layout").exists()) {
-        id("tt_video_ad_close_layout").find().click();
-    }
-    sleep(1000);
-    toastLog("计数器：" + (j + 220));
-}
+// for (j = 0; j < 220; j++) {
+//     // clickable(true).depth(13).find().click();
+//     click("收益加速");
+//     sleep(5000);
+//     if (className("android.widget.ImageView").exists()) {
+//         back();
+//     }
+//     if (id("tt_video_ad_close_layout").exists()) {
+//         id("tt_video_ad_close_layout").find().click();
+//     }
+//     sleep(1000);
+//     toastLog("计数器：" + (j + 220));
+// }
 
 
 sh.exec("am force-stop " + getPackageName("贝壳赚"));
