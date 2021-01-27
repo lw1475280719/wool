0b52a1233379a5f25e7a7e42955352ab448bcb39
JIEMO <2569995914@qq.com>
2021-01-27 22:56:20 +0800

    update

--- js/零钱宝贝.js
+++ js/零钱宝贝.js
@@ -1,22 +1,23 @@
-//(受广告时长影响)耗时40-60分钟
+//耗时2小时
 launch(getPackageName("零钱宝贝"));
 var sh = new Shell(true);
 text("我的钱包").waitFor();
+text("我的钱包").find().click();
 sleep(3000);
-text("领取").find().click();
-for (i = 0; i < 350; i++) {
-    back();
-    id("tt_video_ad_close_layout").find().click();
-    sleep(6500);
-    clickable(true).depth(15).find().click();
-    sleep(1000);
-    if (text("立即提现").exists()) {
-        Tap(540, 1800);//Tap(540,1680);
-        sleep(1000);
-        text("领取").find().click();
+text("立即提现").find().click();
+sleep(2000);
+for (i = 0; i < 1400; i++) {
+    className("android.view.View").text("").find().click();
+    text("立即提现").find().click();
+    className("android.view.View").clickable(true).depth(14).find().click();
+    sleep(5000);
+    if(id("tt_video_ad_close_layout").exists()){
+        id("tt_video_ad_close_layout").find().click();
     }
-    text("领取").find().click();
-    toastLog("计数器：" + (i + 1) + "/350");
+    if (className("android.widget.ImageView").exists()) {
+        back();
+    }
+    toastLog("计数器：" + (i + 1) + "/1400");
 }
 sh.exec("am force-stop " + getPackageName("零钱宝贝"));
 sleep(1000);
