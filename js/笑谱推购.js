103bccdb0c91c0b129482dac0ca1288852b31add
JIEMO <2569995914@qq.com>
2021-02-02 00:11:05 +0800

    add

--- js/笑谱推购.js
+++ js/笑谱推购.js
@@ -1,11 +1,15 @@
-//耗时1小时
+//耗时15分钟（尽量在晚上运行，直播多）
 launch(getPackageName("笑谱推购"));
 var sh = new Shell(true);
 className("androidx.appcompat.app.ActionBar$Tab").clickable(true).selected(true).depth(12).waitFor();
-for (i = 0; i < 330; i++) {
+id("tabTitle").className("android.widget.TextView").text("直播").findOne().parent().parent().click();
+sleep(1000);
+Tap(300, 1400);
+sleep(1000);
+for (i = 0; i < 25; i++) {
     toastLog("计数器：" + (i + 1));
-    sleep(10 * 1000);
-    swipe(500, 1600, 500, 250, 600);
+    sleep(3 * 1000);
+    swipe(500, 1800, 500, 150, 300);
 }
 sh.exec("am force-stop " + getPackageName("笑谱推购"));
 sleep(1000);
