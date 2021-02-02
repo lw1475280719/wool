f662fc2751c8d709fd65b392bf5119b78f68a7d6
JIEMO <2569995914@qq.com>
2021-02-02 22:56:28 +0800

    update

---
+++ js/笑谱推购1.js
@@ -0,0 +1,20 @@
+//耗时15分钟：刷直播（尽量在晚上运行，直播多）
+launch(getPackageName("笑谱推购"));
+var sh = new Shell(true);
+className("androidx.appcompat.app.ActionBar$Tab").clickable(true).selected(true).depth(12).waitFor();
+id("tabTitle").className("android.widget.TextView").text("直播").findOne().parent().parent().click();
+sleep(1000);
+Tap(300, 600);
+sleep(1000);
+for (i = 0; i < 25; i++) {
+    toastLog("计数器：" + (i + 1));
+    if (id("ivClose").exists()) {
+        id("ivClose").find().click();
+    }
+    sleep(30 * 1000);
+    swipe(500, 1800, 500, 150, 300);
+}
+sh.exec("am force-stop " + getPackageName("笑谱推购"));
+sleep(1000);
+sh.exit;
+toastLog("【笑谱推购】已完成计划任务并退出APP！");
--- js/笑谱推购2.js
+++ js/笑谱推购2.js
@@ -1,18 +1,11 @@
-//耗时15分钟：刷直播（尽量在晚上运行，直播多）
+//耗时30分钟:刷视频
 launch(getPackageName("笑谱推购"));
 var sh = new Shell(true);
 className("androidx.appcompat.app.ActionBar$Tab").clickable(true).selected(true).depth(12).waitFor();
-id("tabTitle").className("android.widget.TextView").text("直播").findOne().parent().parent().click();
-sleep(1000);
-Tap(300, 1400);
-sleep(1000);
-for (i = 0; i < 25; i++) {
+for (i = 0; i < 160; i++) {
     toastLog("计数器：" + (i + 1));
-    if (id("ivClose").exists()) {
-        id("ivClose").find().click();
-    }
-    sleep(30 * 1000);
-    swipe(500, 1800, 500, 150, 300);
+    sleep(10 * 1000);
+    swipe(500, 1600, 500, 250, 600);
 }
 sh.exec("am force-stop " + getPackageName("笑谱推购"));
 sleep(1000);
