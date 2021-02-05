63e4291f6456f57c1c1c9f4bb772e9308df78959
JIEMO <2569995914@qq.com>
2021-02-05 14:26:03 +0800

    update

--- js/笑谱推购1.js
+++ js/笑谱推购1.js
@@ -1,16 +1,16 @@
-//耗时15分钟：刷直播（尽量在晚上运行，直播多）
+//耗时15分钟（尽量在晚上运行，直播多）
 launch(getPackageName("笑谱推购"));
 var sh = new Shell(true);
-className("androidx.appcompat.app.ActionBar$Tab").clickable(true).selected(true).depth(12).waitFor();
+className("androidx.appcompat.app.ActionBar$Tab").clickable(true).selected(true).depth(13).waitFor();
 id("tabTitle").className("android.widget.TextView").text("直播").findOne().parent().parent().click();
 sleep(1000);
-Tap(300, 600);
+Tap(300, 1200);
 sleep(1000);
 for (i = 0; i < 25; i++) {
     toastLog("计数器：" + (i + 1));
-    if (id("ivClose").exists()) {
+       if(id("ivClose").exists()){
         id("ivClose").find().click();
-    }
+        }
     sleep(30 * 1000);
     swipe(500, 1800, 500, 150, 300);
 }
