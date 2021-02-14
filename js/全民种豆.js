176d2a9a84ee43a53f51ec775e3984644bdd7730
JIEMO <2569995914@qq.com>
2021-02-14 20:30:26 +0800

    update 全民种豆

--- js/全民种豆.js
+++ js/全民种豆.js
@@ -5,15 +5,14 @@ id("iv_shuoming").waitFor();
 sleep(2000);
 for (i = 0; i < 140; i++) {
     id("iv_yijian").find().click();
-    sleep(3500);
+    sleep(500);
     id("btn_submit").find().click();
     sleep(3000);
     if (!id("iv_shuoming").exists()) {
         id("tt_video_ad_close_layout").find().click();
     }
-    //防互动广告
     swipe(100, 500, 800, 500, 300);
-    if (!id("iv_shuoming").exists()) {
+    if (!id("iv_yijian").exists() && !id("btn_submit").exists()) {
         back();
     }
     if (id("iv_shuoming").exists()) {
