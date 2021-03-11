2a96c4e03b6f471a430464a0160c65a9a2ae86fe
JIEMO <2569995914@qq.com>
2021-03-11 16:15:57 +0800

    update 返利

--- js/返利视频.js
+++ js/返利视频.js
@@ -1,4 +1,4 @@
-//耗时40分钟
+//耗时20分钟
 launchApp("返利");
 var sh = new Shell(true);
 sleep(6000);
@@ -9,7 +9,7 @@ sleep(2000);
 Tap(500, 1000);
 className("android.view.View").text("边看火山热门视频边赚钱").findOne().parent().click();
 sleep(2000);
-for (var i = 1; i <= 210; i++) {
+for (var i = 1; i <= 110; i++) {
     sleep(10 * 1000);
     swipe(800, 1600, 800, 286, 666);
     toastLog("当前进度：" + i + "/210");
--- js/返利阅读.js
+++ js/返利阅读.js
@@ -1,5 +1,4 @@
 //耗时40分钟
-
 launchApp("返利");
 var sh = new Shell(true);
 sleep(6000);
@@ -10,14 +9,14 @@ sleep(2000);
 Tap(540, 1030);
 desc("去赚钱").findOne().click();
 sleep(5000);
-for (var i = 1; i <= 34; i++) {
+for (var i = 1; i <= 23; i++) {
     Tap(245, 450);
-    sleep(65 * 1000);
+    sleep(93 * 1000);
     id("iv_left").find().click();
     sleep(1000);
     swipe(800, 450, 800, 1450, 666);
     sleep(3000);
-    toastLog("当前进度：" + i * 2 + "/34");
+    toastLog("当前进度：" + (i/23*100).toFixed(2)+"%");
 }
 sh.exec("am force-stop " + getPackageName("返利"));
 sleep(1000);
