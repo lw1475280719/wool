98072f82cd507f5f9c0909e82d48c055cf11ceb8
JIEMO <2569995914@qq.com>
2021-03-12 15:30:05 +0800

    update 返利

--- js/返利阅读.js
+++ js/返利阅读.js
@@ -9,14 +9,14 @@ sleep(2000);
 Tap(540, 1030);
 desc("去赚钱").findOne().click();
 sleep(5000);
-for (var i = 1; i <= 23; i++) {
+for (var i = 1; i <= 33; i++) {
     Tap(245, 450);
-    sleep(93 * 1000);
+    sleep(63 * 1000);
     id("iv_left").find().click();
     sleep(1000);
     swipe(800, 450, 800, 1450, 666);
     sleep(3000);
-    toastLog("当前进度：" + (i / 23 * 100).toFixed(2) + "%");
+    toastLog("当前进度：" + (i / 33 * 100).toFixed(2) + "%");
 }
 sh.exec("am force-stop " + getPackageName("返利"));
 sleep(1000);
