ee87b93097ae3f923a8bdd67889a5c5060245d68
JIEMO <2569995914@qq.com>
2021-10-10 18:53:36 +0800

    add

---
+++ js/快音.js
@@ -0,0 +1,28 @@
+//耗时3小时
+APP_name = "快音";
+
+function start() {
+    launch(getPackageName(APP_name));
+    var sh = new Shell(true);
+    return sh;
+}
+
+function stop(sh) {
+    sh.exec("am force-stop " + getPackageName(APP_name));
+    sleep(1000);
+    sh.exit;
+    toastLog("【" + APP_name + "】已完成计划任务并退出APP！");
+}
+
+function handle() {
+    sleep(30*3600*1000);
+}
+
+start1 = start()
+handle()
+stop(start1)
+
+
+
+
+
